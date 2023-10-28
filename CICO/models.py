4abbde2c45cc69b83c2c1dcf6d471b81690623ce
Lucas Pironet <l.pironet@students.ephec.be>
2023-10-28 16:12:46 +0200

    Création des tables requises par le projet
    Qui: Lucas, sur base du diagrame UML fait par...Lucas, oui j'ai fait bcp de choses
    Je teste un format de commit message
    
    Quoi:
    usercico: la table crée par django pour gerer les profils
    
    table CICO_usersettings: table qui stocke les reglages d'un utilisateur (notifications, préférences, customisation, etc)
    
    table CICO_deviceRecords: table qui stocke ce que chaque module enregistre, le passage, l'heure, si l'animal est un chat reconnu ou inconnu
    
    table CICO_trigger: si l'animal qui a déclenché (to trigger) le module est un chat reconnu, quel chat est-il, cette table est nécessaire, car on est dans une association 0-1
    
    table CICO_cats: table qui stocke les chats, leur propriétaire et leurs détails
    
    Pourquoi:
    Afin de déjà avoir un modèle que tout le monde peut utiliser sans devoir a chaque fois creer la table appropriée
    
    Comment:
    Modification du fichier models pour inclure les modeles correspondants aux tables cats, deviceRecords, trigger, userSettings
    
    Creation du fichier de migration les implémentant dans la db

--- CICO/models.py
+++ CICO/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
+from django.db.models import UniqueConstraint
 
 
 class UserCICO(AbstractUser):
@@ -25,3 +26,34 @@ class Statuses(models.Model):
     def __str__(self):
         return f"{self.status} à {self.hour}"
 
+
+class UserSettings(models.Model):
+    userId = models.OneToOneField(UserCICO, primary_key=True, on_delete=models.PROTECT)
+    setting1 = models.CharField(max_length=100)
+    #add other settings as required
+
+class DeviceRecords(models.Model):
+    deviceId = models.ForeignKey(UserCICO, to_field="ownedDevice", on_delete=models.PROTECT)
+    recordId = models.AutoField(primary_key=True)
+    UniqueConstraint(fields=['deviceId', 'recordId'], name='composite_PK')
+    time = models.DateTimeField(auto_now_add=True)
+
+    EVENTS_CHOICES = [
+        ("IN", "Entrée"),
+        ("OUT", "Sortie"),
+    ]
+    event = models.CharField(max_length=3, choices=EVENTS_CHOICES)
+    isCat = models.BooleanField()
+
+class Cats(models.Model):
+    ...
+    ownerId = models.ForeignKey(UserCICO, on_delete=models.CASCADE)
+    catId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #add other details if needed
+
+
+class Trigger(models.Model):
+    ...
+    catId = models.ForeignKey(Cats, on_delete=models.CASCADE)
+    recordId = models.ForeignKey(DeviceRecords,primary_key=True,  to_field="recordId", on_delete=models.CASCADE)

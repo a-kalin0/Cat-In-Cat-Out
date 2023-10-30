e48dfd21f211e2d804792b8d914fc2484e4907e5
Lucas Pironet <l.pironet@students.ephec.be>
2023-10-30 16:02:18 +0100

    #62 Réalisation de l'US03, historique des entrées-sorties
    Qui: Lucas (PirLucas)
    
    Quoi: US 3: En tant qu'utilisateur, je veux avoir acces a un historique des entrées et sorties de tout mes chat dès ma connexion, afin d'avoir un apperçu rapide du comportement de mes chats.
    
    Ajout sur la page d'accueil du profil utilisateur d'une liste générée à partir des enregistrement que sa chatière. Cette liste contiens dans l'ordre chronologique descendant tout les passages enregistrés.
    Un nombre limité d'élément s'affiche a la fois, l'utilisateur dispose de deux boutons pour naviguer dans le reste de la liste.
    
    Pourquoi: Cette fonctionnalité a été rajoutée pour permettre à l'utilisateur d'avoir un apreçu rapide de tout les passages récents de ses chats dès qu'il se connecte
    
    Comment:
    
    urls.py: L'URL "profileIndex" a été modifiée pour utiliser un parametre "listButton", ce parametre contiens une chaine de caractère envoyée par un des deux boutons ajoutés à la page profileIndex.html, il est utilisé par la vue profileIndex pour obtenir une nouvelle liste a afficher à l'écran. Ce parametre doit valoir "None" quand on accede à cette page sans avoir utilisé un de ces deux boutons.
    
    views.py:
    -Création de la constante LIST_SIZE, qui représente la ne nombre d'éléments de la liste qui seront affiché sur la page
    
    -Création de la fonction "GetRecords" qui cherche dans la DB l'ensemble des enregistrements associés à un deviceId (=un utilisateur unique) et retourne le résultat sous forme de liste
    
    -Création de la variable de session "listStart" qui représente l'indice du premier élément qui sera affiché à l'écran. Cet indice est incrémenté ou décrémenté en fonction du bouton de navigation utilisé. Si la décrémentation ou incrémentation sortirait de la liste des enregistrements, sa valeur reviens à son état précédent.
    
    -Création de la fonctione "UpdateList" qui utilise le retour de GetRecords, l'inverse afin d'avoir la liste dans l'ordre chronologique décroissant (du plus récent au plus ancien) et retourne une nouvelle liste de longueur LIST_SIZE qui commence à partir de l'indice contenu dans la variable de session "listStart"
    
    -Modification de profileIndex afin d'utiliser le parametre "listButton" envoyé dans l'url. Le parametre "récent" décrémente la valeur de "listStart" de LIST_SIZE, afin d'obtenir les éléments plus récent de la liste d'énregistrements. Le parametre "ancien" incrémente la valeur de "listStart" de LIST_SIZE.
    La variable "recordList" contiens le retour de la fonction "UpdateList", cette variable est envoyée à la page profileIndex.html pour être affichée.
    
    templates/profileIndex.html:
    
    -Ajout d'une boucle parcourant la liste recordList qui pour chaque élément crée une cellule de tableau contenant le nom du chat, l'évenement et la date
    -Ajout des boutons de navigations redirigeant sur la même page, avec le paramètre utilisé par la vue "profileIndex"
    
    notes:
    - Seul le contenu de la liste qui est affiché est envoyé, afin de ne pas demander toute la liste des enregistrements a chaque fois mais ça implique que la navigation dans la liste rafraichisse toute la page, car ce n'est pas possible de modifier dynamiquement le contenu de celle-ci sans utiliser du JS pour faire une requête ajax

--- CICO/views.py
+++ CICO/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView
 from .models import CiCoItem
 from .models import Statuses
 from .models import UserCICO
+from .models import Cats
+from .models import DeviceRecords
+from .models import Trigger
 from django.shortcuts import redirect
 from CICO.forms import ContactUsForm
 from CICO.forms import ConnectionForm
@@ -15,6 +18,21 @@ from django.contrib.auth import logout
 logger = logging.getLogger('django')
 from django.http import HttpResponse
 
+from django.db.models import F
+
+LIST_SIZE = 2
+
+def UpdateList(request, deviceId):
+    recordList = GetRecords(deviceId)[::-1]
+    newList = recordList[request.session['listStart']:request.session['listStart'] + LIST_SIZE]
+    if len(newList) == 0:
+        newList = recordList[request.session['listStart'] - LIST_SIZE:request.session['listStart']]
+        request.session['listStart'] -= LIST_SIZE
+    return newList
+
+def GetRecords(deviceId):
+    querySet = DeviceRecords.objects.all().annotate(catName=F('trigger__catId__name'))
+    return querySet.values()
 
 def Empty(request):
     return redirect("CICO/")
@@ -38,7 +56,6 @@ def vue(request):
 
 
 def connection(request, formId):
-
     if (formId == 0):
         logout(request)
         request.session['IP'] = ""
@@ -48,13 +65,14 @@ def connection(request, formId):
         if (request.method == "POST"):
             form = ConnectionForm(request.POST)
             if form.is_valid():
-                #newItem = CiCoItem(text=form.cleaned_data["message"])
-                #newItem.save()
-                user = authenticate(username=form.cleaned_data["identification"], password=form.cleaned_data["password"])
+                # newItem = CiCoItem(text=form.cleaned_data["message"])
+                # newItem.save()
+                user = authenticate(username=form.cleaned_data["identification"],
+                                    password=form.cleaned_data["password"])
                 if user is not None:
                     login(request, user)
                     request.session['IP'] = request.META.get("REMOTE_ADDR")
-                    request.session['user'] = user.id                # A backend authenticated the credentials
+                    request.session['user'] = user.id  # A backend authenticated the credentials
                     return redirect('profileIndex')
                 else:
                     # No backend authenticated the credentials
@@ -65,8 +83,8 @@ def connection(request, formId):
         if (request.method == "POST"):
             form = NewAccountForm(request.POST)
             if form.is_valid():
-                if form.cleaned_data["email"] in UserCICO.objects.values_list("email", flat = True):
-                    logger.info("This email is already used") #these texts will need to be displayed on the page
+                if form.cleaned_data["email"] in UserCICO.objects.values_list("email", flat=True):
+                    logger.info("This email is already used")  # these texts will need to be displayed on the page
                 elif form.cleaned_data["password"] != form.cleaned_data["confirmPassword"]:
                     logger.info("Passwords not identical")
                 else:
@@ -75,8 +93,7 @@ def connection(request, formId):
                                                       username=form.cleaned_data["identification"])
                     newUser.set_password(form.cleaned_data["password"])
                     newUser.save()
-                    request.session['user'] = newUser.id  # A backend authenticated the credentials
-                    return redirect('profileIndex')
+                    return redirect('profileIndex/None')
         else:
             form = NewAccountForm()
 
@@ -87,7 +104,7 @@ def connection(request, formId):
             if form.is_valid():
                 emails = UserCICO.objects.values_list('email')
                 if form.cleaned_data["email"] in emails:
-                    #send mail
+                    # send mail
                     ...
         else:
             form = RequestNewPasswordForm()
@@ -95,18 +112,28 @@ def connection(request, formId):
     return render(request, 'CICO/connexion.html', {"form": form})
 
 
-def profileIndex(request):
+def profileIndex(request, listButton="None"):
+    if listButton == "None":
+        request.session['listStart'] = 0
+    elif listButton == "récent":
+        request.session['listStart'] = max(0, request.session[
+            'listStart'] - LIST_SIZE)  # the max function is used to prevent the substraction from resulting in a negative
+    elif listButton == "ancien":
+        request.session['listStart'] += LIST_SIZE
+
     if not checkIP(request):
         return render(request, 'CICO/unauthorized.html', status=401)
     items = CiCoItem.objects.all()
     print(request.user)
     print(request.META.get("REMOTE_ADDR"))
     user = request.user
+    recordList = UpdateList(request, UserCICO.objects.get(username=request.user).ownedDevice)
+
     if request.user.is_authenticated:
-        return render(request, 'CICO/profileIndex.html', {"items": items, "user": user.username})
+        return render(request, 'CICO/profileIndex.html',
+                      {"items": items, "user": user.username, "recordList": recordList})
     else:
         return render(request, 'CICO/unauthorized.html', status=401)
-
 
 
 def faq(request):
@@ -126,7 +153,7 @@ class PageMotE(ListView):
     template_name = "CICO/pageE.html"
 
     def get_queryset(self):
-        return CiCoItem.objects.filter(text__icontains= "e")
+        return CiCoItem.objects.filter(text__icontains="e")
 
 
 class PageStatus(ListView):

fc1b54aa1ba8a6face635bf313320d63c19fc598
Carlos Iglesias <iglesias1602@gmail.com>
2023-11-01 17:08:53 +0100

    Cats are added correctly to the DB #20
    The cats are added correctly to the database.
    
    There were some files that were modified for the logic to add the cats to the database.
    
    The files that were modified are:
        - CICO/models.py
        the path to add the cat images are correctly set. The function
        "def cat_directory_path(instance, filename):
        return 'media/user_{0}/cat_{1}/{2}'.format(instance.ownerId_id, instance.catId, filename)"
    
        Is being used to set the path to the cat images in the model Cats:
        "image = models.ImageField(upload_to=cat_directory_path..."
        the upload_to parameter is set to the function cat_directory_path.
        The file system that was thought to be implemented is working correctly.
    
        Now we have to do some research on how to implement the file system in the server
        and what are the security measures that we have to take to avoid any kind of attack.
    
        - CICO/static/CICO/js/showCatImg.js
        The function "showCatImg" was modified to show the cat images correctly.
    
        There was an error shown in the console because the function was being called
        even if we didn't have any cat image to show. Now the function is called only
        if the cat image exists. In other words, it is called when we click on the upload
        button and it uses the parameter onchange to call the function.
    
        - CICO/views.py
        The function "def addCat(request):" was modified to add the cat to the database.
        The function is called when we click on the 'ajouter' button.
        The function is called with the parameter "request.POST" and the cat image is
        called with the parameter "request.FILES['catImage']". The cat image is saved
        in the file system with the function "cat.save()".
    
        The "return JsonResponse({'success': True, 'catName' : cat.name})" is used to
        return a json response to the ajax call in the template profileIndex.html.
    
        It serves debugging purposes. It has to be removed in the future.
    
        - templates/CICO/profileIndex.html
        The template was modified to add the cat image to the page.
    
        Fields in <tbody> were removed.
    
        The <input> parameter for the images has been modified to call the script
        "showCatImg.js" when the image is uploaded.
    
        Two "toast" containers where added to show if the cat was added correctly or not.
    
        The script to submit the form was modified, if the submission is successful
        the toast container is shown and the cat field is added to the page.
        If the submission is not successful, the toast container is shown and the
        cat field is not added to the page.
    
        It is just adding this directly if the submission is successful. It is not
        fully functional, if the page is recharged it will not show the cat fields
        that were added, because I have just added the logic so the cats are added
        correctly to the DB.
    
        *** The next step is to load the data from the cats for the user ***
    
        - templates/baseProfil.html
        the script "showCatImg.js" was added to the <head> of the base template.

--- CICO/static/CICO/js/showCatImg.js
+++ CICO/static/CICO/js/showCatImg.js
@@ -1,19 +1,13 @@
-function updateImagePreview(inputId, imageId) {
-    document.getElementById(inputId).addEventListener('change', function (event) {
-        const input = event.target;
-        if (input.files && input.files[0]) {
-            const reader = new FileReader();
+function updateImagePreview(input, imageId) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
 
-            reader.onload = function (e) {
-                document.getElementById(imageId).src = e.target.result;
-            }
-
-            reader.readAsDataURL(input.files[0]);   
+        reader.onload = function (e) {
+            document.getElementById(imageId).src = e.target.result;
         }
-    });
+
+        reader.readAsDataURL(input.files[0]);   
+    }
 }
 
-updateImagePreview('imgFrontInput', 'imgFrontPreview');
-updateImagePreview('imgBackInput', 'imgBackPreview');
-updateImagePreview('imgLeftInput', 'imgLeftPreview');
-updateImagePreview('imgRightInput', 'imgRightPreview');
+window.updateImagePreview = updateImagePreview;

3caa844404604105cc038ba0c4ea915f9292b80e
Antho3004 <t.saragossi@students.ephec.be>
2023-11-23 10:56:56 +0100

    TT1: connexion raspery--> modification du code (envoie fonctionnel)

--- CICO/views.py
+++ CICO/views.py
@@ -11,11 +11,13 @@ import logging
 from django.contrib.auth import authenticate
 logger = logging.getLogger('django')
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def postRaspberry(request):
     if request.method == 'POST':
-        print(request.POST)
-        return None 
+        print(request.FILES)
+        return HttpResponse("test") 
 
 def Empty(request):
     return redirect("CICO/")

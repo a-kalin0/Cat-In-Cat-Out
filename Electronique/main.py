6b703538b246be6ce4fb7cd5d17a4e6bea51cc52
Antho3004 <t.saragossi@students.ephec.be>
2023-11-30 11:11:39 +0100

    mode veille V1

--- Electronique/main.py
+++ Electronique/main.py
@@ -1,49 +1,40 @@
 from gpiozero import MotionSensor
-from picamera import PiCamera
+from picamera2 import Picamera2
 import time
 
 # Temps en secondes après lequel la caméra sera éteinte en l'absence de mouvement
-TEMPS_INACTIVITE_MAX = 300
-
-#def capture_photo():
-    # Implémentez la logique pour prendre une photo
-    #pass
+TEMPS_INACTIVITE_MAX = 10
 
 def main():
     pir = MotionSensor(4)
-    camera = PiCamera()
-    camera.start_preview()  # Démarrez la prévisualisation
-
-    dernier_mouvement = time.time()
+    camera = Picamera2() 
 
     print("Démarrage du script")
 
-    try:
-        while True:
-            if pir.motion_detected:
-                print("Mouvement détecté !")
-                capture_photo()
-                dernier_mouvement = time.time()
+    time_since_motion = 0  # Initialiser la variable à l'extérieur de la boucle
+    camera_active = True
+    camera.start_preview()
 
-            # Vérifiez le temps écoulé depuis le dernier mouvement
-            temps_inactivite = time.time() - dernier_mouvement
-
-            if temps_inactivite > TEMPS_INACTIVITE_MAX:
-                # Éteignez la caméra en cas d'inactivité
-                camera.stop_preview()
-                print("Mode veille activé.")
-                while not pir.motion_detected:
-                    time.sleep(1)
-                # Redémarre la caméra lorsque le mouvement est détecté
+    while True:
+        if pir.motion_detected:
+            print("Mouvement détecté!")
+            time_since_motion = 0 # Réinitialiser le compteur de temps d'inactivité
+            if not camera_active:
+                camera = Picamera2()
                 camera.start_preview()
-                dernier_mouvement = time.time()
+                camera_active = True
+                print("la caméra est réactiver")
+        else:
+            time.sleep(1)  # Attendre une seconde
+            time_since_motion += 1
 
-    except KeyboardInterrupt:
-        # Gestion de l'arrêt du script avec Ctrl+C
-        print("Arrêt du script")
-    finally:
-        camera.stop_preview()
-        camera.close()
+            print(time_since_motion)
+            if camera_active and time_since_motion >= TEMPS_INACTIVITE_MAX:
+                print(f"Aucun mouvement depuis {TEMPS_INACTIVITE_MAX} secondes. Désactivation de la caméra.")
+                camera.close()
+                camera_active = False
+
 
 if __name__ == "__main__":
     main()
+

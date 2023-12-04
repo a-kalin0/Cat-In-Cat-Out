c5ecad21ceded56a130890c10777b34d8f6b40a0
o-kinnen <t.saragossi@students.ephec.be>
2023-12-04 16:12:04 +0100

    modification de send_data afin de pouvoir envoyer des données automatiquement

--- Eletronique/send_data.py
+++ Eletronique/send_data.py
@@ -1,24 +1,21 @@
 import requests
 import json
 
+
 url = "http://192.168.43.11:8000/CICO/postRaspberry"
-
 #url = "http://172.20.10.6:8000/CICO/postRaspberry"
+def send_data(dictionnaire) :
+    # Les données textuelles
+    data = {
+        "donneTest": "test"
+    }
 
+    # La photo à envoyer
+    dictionnaire 
 
-# Les données textuelles
-data = {
-    "donneTest": "test"
-}
-
-# La photo à envoyer
-files = {
-    'photo': open('/home/cico/Cat-In-Cat-Out/Eletronique/test.jpg', 'rb')
-}
-
-#token et cookies
-headers = {'X-CSRFToken': 'votre_token_csrf'}
-cookies = {'csrftoken': 'votre_token_csrf'}
-# Effectuer la requête POST
-response = requests.post(url, data=data, files=files, headers=headers, cookies=cookies)
-print(response.text)
+    #token et cookies
+    headers = {'X-CSRFToken': 'votre_token_csrf'}
+    cookies = {'csrftoken': 'votre_token_csrf'}
+    # Effectuer la requête POST
+    response = requests.post(url, data=data, dictionnaire=dictionnaire, headers=headers, cookies=cookies)
+    print(response.text)

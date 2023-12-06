4c84e7e3d7d4ee0345077e3e92d897cd19a631c8
o-kinnen <t.saragossi@students.ephec.be>
2023-12-06 14:27:25 +0100

    suppression des fichiers et dossiers inutiles

--- Electronique/Pictures/picture.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:8000/'
-files = {'file': open('/home/cico/Cat-In-Cat-Out/Electronique/Pictures/test0.jpg', 'rb')}
-
-response = requests.post(url, files=files)

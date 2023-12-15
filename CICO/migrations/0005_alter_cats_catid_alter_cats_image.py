adb6fa405d1027133b2385238ca16a6fce620e49
Carlos Iglesias <92795126+iglesias1602@users.noreply.github.com>
2023-12-15 12:37:15 +0100

    Fix branch workflow after accidental delete

--- CICO/migrations/0005_alter_cats_catid_alter_cats_image.py
+++ CICO/migrations/0005_alter_cats_catid_alter_cats_image.py
@@ -2,7 +2,7 @@
 
 import CICO.models
 from django.db import migrations, models
-import uuid
+import uuid6
 
 
 class Migration(migrations.Migration):
@@ -12,11 +12,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='cats',
-            name='catId',
-            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
-        ),
         migrations.AlterField(
             model_name='cats',
             name='image',
